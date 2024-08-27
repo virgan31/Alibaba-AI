77de05dc49ddaa380c7b9d91e6f7e2b6020b2fc3
Farras Virgana Azis <eklesia945@gmail.com>
2024-08-27 23:47:47 +0700

    Qwen Max

--- test.py
+++ test.py
@@ -1,32 +1,27 @@
-import random
 from http import HTTPStatus
 import dashscope
 
 dashscope.base_http_api_url = 'https://dashscope-intl.aliyuncs.com/api/v1'
 
-def call_stream_with_messages():
+def call_with_stream():
     messages = [
-        {'role': 'user', 'content': 'Tell Me About PT. Dwi Tunggal Putra'}]
-    responses = dashscope.Generation.call(
-        'qwen1.5-7b-chat',
-        messages=messages,
-        seed=random.randint(1, 10000),  # set the random seed, optional, default to 1234 if not set
-        result_format='message',  # set the result to be "message"  format.
-        stream=True,
-        output_in_full=True  # get streaming output incrementally
-    )
-    full_content = ''
+        {'role': 'user', 'content': 'Introduce the capital of China'}]
+    responses = dashscope.Generation.call("qwen-max",
+                                messages=messages,
+                                result_format='message',  # set the result to be "message"  format.
+                                stream=True, # set streaming output
+                                incremental_output=True  # get streaming output incrementally
+                                )
     for response in responses:
         if response.status_code == HTTPStatus.OK:
-            full_content += response.output.choices[0]['message']['content']
-            print(response)
+            print(response.output.choices[0]['message']['content'],end='')
         else:
             print('Request id: %s, Status code: %s, error code: %s, error message: %s' % (
                 response.request_id, response.status_code,
                 response.code, response.message
             ))
-    print('Full content: \n' + full_content)
 
 
 if __name__ == '__main__':
+    call_with_stream()
     call_stream_with_messages()
